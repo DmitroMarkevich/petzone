fb07830cf0053f7ce2f86b58bb781041ade35154
Dmytro Markevych <119111318+DmitroMarkevich@users.noreply.github.com>
2025-03-13 15:37:03 +0200

    fixed bug with non-working edit button in address section

--- resources/js/profile/profile-edit.js
+++ resources/js/profile/profile-edit.js
@@ -13,27 +13,48 @@ $(document).ready(function () {
         });
     };
 
-    const toggleButtons = (edit, save, cancel, editing) => {
-        edit.toggle(!editing);
-        save.add(cancel).toggle(editing);
+    // Function to toggle visibility of the edit, save, and cancel buttons
+    const toggleButtonsVisibility = (edit, save, cancel, isEditing) => {
+        edit.toggle(!isEditing);
+        save.add(cancel).toggle(isEditing);
     };
 
-    const toggleEditForm = (edit, save, cancel, inputs, excluded) => {
-        edit.on('click', (e) => {
+    // Function to handle toggling of edit form
+    const handleEditFormToggle = (editButton, saveButton, cancelButton, formInputs, excludedFields) => {
+        // Enable edit mode when the edit button is clicked
+        editButton.on('click', (e) => {
             e.preventDefault();
-            toggleFormInputs(inputs, excluded, false);
-            toggleButtons(edit, save, cancel, true);
+            toggleFormInputs(formInputs, excludedFields, false);
+            toggleButtonsVisibility(editButton, saveButton, cancelButton, true);
         });
-        cancel.on('click', () => {
-            toggleFormInputs(inputs, excluded, true);
-            toggleButtons(edit, save, cancel, false);
+
+        // Cancel the editing when cancel button is clicked
+        cancelButton.on('click', () => {
+            toggleFormInputs(formInputs, excludedFields, true);
+            toggleButtonsVisibility(editButton, saveButton, cancelButton, false);
         });
     };
 
+    // Store the original values of all form inputs when the page loads
     $('.my-data input, .delivery-address input').each(function () {
         $(this).data('original-value', $(this).val());
     });
 
-    toggleEditForm($('#edit-profile'), $('#save-profile'), $('#cancel-profile'), $('.my-data input'), ['email']);
-    toggleEditForm($('#edit-address'), $('#save-address'), $('#cancel-address'), $('.delivery-address input'));
+    // Handle toggling for profile section
+    handleEditFormToggle(
+        $('#edit-profile'),
+        $('#save-profile'),
+        $('#cancel-profile'),
+        $('.my-data input'),
+        ['email']
+    );
+
+    // Handle toggling for delivery address section
+    handleEditFormToggle(
+        $('#edit-address'),
+        $('#save-address'),
+        $('#cancel-address'),
+        $('.delivery-address input'),
+        [] // No exclusions for address fields
+    );
 });
