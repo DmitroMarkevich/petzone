82519aad7b6fcdf68276ead8864618026f4e13ed
Dmytro Markevych <119111318+DmitroMarkevich@users.noreply.github.com>
2025-03-13 15:57:27 +0200

    moved inline styles to home.scss file

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -62,23 +62,21 @@
             <div class="form-row">
                 <h2 class="section-title">Акційні товари</h2>
                 <a href="" class="view-all-button">Дивитись усе
-                    <img src="{{ asset('images/right-arrow.svg') }}" alt="">
+                    <img src="{{ asset('images/right-arrow.svg') }}" alt=">">
                 </a>
             </div>
-
-            <div class="promo-adverts">
-
-            </div>
+            <ul class="promo-adverts">
+            </ul>
         </section>
 
         <section class="content-section">
             <div class="form-row">
                 <h2 class="section-title">Свіжі пропозиції</h2>
                 <a href="" class="view-all-button">Дивитись усе
-                    <img src="{{ asset('images/right-arrow.svg') }}" alt="">
+                    <img src="{{ asset('images/right-arrow.svg') }}" alt=">">
                 </a>
             </div>
-            <ul class="" style="display: flex; justify-content: center; gap: 16px">
+            <ul class="fresh-adverts-list">
                 @foreach($freshAdverts as $advert)
                     <li>
                         @include('components.advert-card', ['adverts' => $advert])
@@ -93,23 +91,29 @@
                 <div class="delivery-step-item">
                     <img src="{{ asset('images/home/01.svg') }}" alt="">
                     <h4 class="delivery-step-title">Переглянь оголошення</h4>
-                    <p class="delivery-step-description">Знайди ідеальний продукт для свого вихованця, який відповідає
-                        всім твоїм потребам.</p>
+                    <p class="delivery-step-description">
+                        Знайди ідеальний продукт для свого вихованця, який відповідає всім твоїм потребам.
+                    </p>
                 </div>
                 <div class="delivery-step-item">
                     <h4 class="delivery-step-title">Обери спосіб доставки</h4>
-                    <p class="delivery-step-description">Вибери зручний спосіб доставки: Нова Пошта або Укрпошта.</p>
+                    <p class="delivery-step-description">
+                        Вибери зручний спосіб доставки: Нова Пошта або Укрпошта.
+                    </p>
                     <img src="{{ asset('images/home/02.svg') }}" alt="">
                 </div>
                 <div class="delivery-step-item">
                     <img src="{{ asset('images/home/03.svg') }}" alt="">
                     <h4 class="delivery-step-title">Відстежуй своє замовлення</h4>
-                    <p class="delivery-step-description">Отримай номер для відстеження та контролюй кожен крок процесу
-                        доставки.</p>
+                    <p class="delivery-step-description">
+                        Отримай номер для відстеження та контролюй кожен крок процесу доставки.
+                    </p>
                 </div>
                 <div class="delivery-step-item">
                     <h4 class="delivery-step-title">Забери та оплати</h4>
-                    <p class="delivery-step-description">Забери замовлення на пошті, оглянь та оплати при отриманні.</p>
+                    <p class="delivery-step-description">
+                        Забери замовлення на пошті, оглянь та оплати при отриманні.
+                    </p>
                     <img src="{{ asset('images/home/04.svg') }}" alt="">
                 </div>
             </div>
@@ -123,15 +127,15 @@
                     <h4 class="why-us-title">Продукти високої якості</h4>
                 </div>
                 <div class="why-us-item">
-                    <img src="{{asset('images/home/sellers.png')}}" alt="Прямий зв'язок з продавцями">
+                    <img src="{{ asset('images/home/sellers.png') }}" alt="Прямий зв'язок з продавцями">
                     <h4 class="why-us-title">Прямий зв'язок з продавцями</h4>
                 </div>
                 <div class="why-us-item">
-                    <img src="{{asset('images/home/choice.png')}}" alt="Широкий вибір">
+                    <img src="{{ asset('images/home/choice.png') }}" alt="Широкий вибір">
                     <h4 class="why-us-title">Широкий вибір</h4>
                 </div>
                 <div class="why-us-item">
-                    <img src="{{asset('images/home/delivery.png')}}" alt="Гнучкі варіанти доставки">
+                    <img src="{{ asset('images/home/delivery.png') }}" alt="Гнучкі варіанти доставки">
                     <h4 class="why-us-title">Гнучкі варіанти доставки</h4>
                 </div>
                 <div class="why-us-item">
