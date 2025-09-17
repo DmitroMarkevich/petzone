80ee4d31aac65376e104ac5b5fb687c2e5839aff
Dmytro Markevych <119111318+DmitroMarkevich@users.noreply.github.com>
2025-09-17 19:22:21 +0300

    Stop tracking cached view files

--- storage/framework/views/f1761f7ad378ccf7efa67615ab14518b.php
+++
@@ -1,7 +0,0 @@
-<?php $__env->startSection('title', '404'); ?>
-
-<?php $__env->startSection('app-content'); ?>
-    <p>error 404</p>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layouts.app', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH C:\Users\dmark\PhpstormProjects\petzone\resources\views/errors/404.blade.php ENDPATH**/ ?>
