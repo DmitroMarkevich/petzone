0ef360acebbe7dcc9f864f82d1ad0eb990a65897
Dmytro Markevych <119111318+DmitroMarkevich@users.noreply.github.com>
2025-09-18 15:24:03 +0300

    add eager loading for first slider image to avoid alt flicker

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -8,27 +8,33 @@
             </a>
 
             <div class="image-slider"
-                x-data="{
-                currentIndex: 0,
-                sliderImages: [
-                    { src: '/images/auth/shopping-cart.png', text: '{{ __('auth.slider.shopping_cart') }}' },
-                    { src: '/images/auth/carton.png', text: '{{ __('auth.slider.carton') }}' },
-                    { src: '/images/auth/receipt.png', text: '{{ __('auth.slider.receipt') }}' }
-                ],
-                init() {
-                    setInterval(() => {
-                        this.currentIndex = (this.currentIndex + 1) % this.sliderImages.length;
-                    }, 5000);
-                }}"
+                 x-data="{
+                    currentIndex: 0,
+                    sliderImages: [
+                        { src: '/images/auth/shopping-cart.png', text: '{{ __('auth.slider.shopping_cart') }}' },
+                        { src: '/images/auth/carton.png', text: '{{ __('auth.slider.carton') }}' },
+                        { src: '/images/auth/receipt.png', text: '{{ __('auth.slider.receipt') }}' }
+                    ],
+                    init() {
+                        setInterval(() => {
+                            this.currentIndex = (this.currentIndex + 1) % this.sliderImages.length;
+                        }, 5000);
+                    }
+                 }"
             >
                 <div class="slider-frame">
-                    <img :src="sliderImages[currentIndex].src" alt="Slider Image" class="slider-image">
+                    <img
+                        src="{{ asset('/images/auth/shopping-cart.png') }}"
+                        :src="sliderImages[currentIndex].src"
+                        alt="Slider Image"
+                        class="slider-image">
+
                     <p class="tagline" x-text="sliderImages[currentIndex].text"></p>
                 </div>
 
                 <div class="slider-dots">
                     <template x-for="(image, index) in sliderImages" :key="index">
-                    <span class="dot" :class="{ 'active': currentIndex === index }"></span>
+                        <span class="dot" :class="{ 'active': currentIndex === index }"></span>
                     </template>
                 </div>
             </div>
